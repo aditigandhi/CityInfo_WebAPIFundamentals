583cc70d5c68712da4d0d14dba480cc554188d53
Aditi Gandhi <Aditi.Gandhi@bd.com>
2022-08-18 11:56:18 -0700

    Routing, Controllers, Content Negotiation, Returning/Downloading File

---
+++ CityInfo.API/Models/CityDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CityDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string? Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDTO> PointsOfInterest { get; set; } = new List<PointOfInterestDTO>();
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,25 +1,41 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true; // when consumer asks for option that we do not have configured
+}).AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) // only adds swagger middleware when running in dev env
+{ //Middleware -- swagger generalizaion used in swagger ui 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
+
+///////////////////////////////////////// NOTES ///////////////////////////////////////////////////
+
+/* Model View Controller
+ * Loose coupling, separation of pattern
+ * Model: app logic
+ * View: displaying data
+ * Controller: handling interaction between model and view
+ */
